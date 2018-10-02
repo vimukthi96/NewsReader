0016765db64141270bd7d920eb278f603990d060
vimukthi96 <38091051+vimukthi96@users.noreply.github.com>
2018-10-02 21:48:01 +0530

    Update NewsServices.java

--- app/src/main/java/com/example/vimukthi/newsreader/Interfaces/NewsServices.java
+++ app/src/main/java/com/example/vimukthi/newsreader/Interfaces/NewsServices.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface NewsServices {
-    @GET("v2/sources?apiKey=API_KEY")
+    @GET("v2/sources?apiKey=API_KEY")//this api is wrong
     Call<WebSite>getSources();
 
 }
